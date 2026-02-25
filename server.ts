52974df02ec79a92502f159fb02152220d7b45e1
Vieirah <guiilhermev99@gmail.com>
2026-02-24 21:04:38 -0300

    Update

--- server.ts
+++ server.ts
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 3001;
 
 // Middlewares
 // Limite de 50mb é fundamental para que as imagens em Base64 não deem erro 'Payload Too Large'
-app.use(cors()); 
+app.use(cors({
+  origin: 'https://acade-me-frontend.vercel.app/',
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
